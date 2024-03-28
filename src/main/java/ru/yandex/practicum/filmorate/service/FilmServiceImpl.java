b77ebbe0af8d91a6f9730733ac784ff134410032
LuckyLoki <luckyloki666@ya.ru>
2024-03-28 21:30:22 +0400

    10.2.1 fixed according to the comments

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.ecxeption.NotFoundException;
 import ru.yandex.practicum.filmorate.ecxeption.RepeatException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
@@ -45,12 +44,7 @@ public class FilmServiceImpl implements FilmService {
 
     @Override
     public Film addLike(int filmId, int userId) {
-        if (filmStorage.getById(filmId) == null) {
-            throw new NotFoundException("No film with such id");
-        }
-        if (userService.getById(userId) == null) {
-            throw new NotFoundException("No user with such id");
-        }
+        userService.getById(userId);
         Film film = filmStorage.getById(filmId);
         if (film.getLikes().contains(userId)) {
             throw new RepeatException("Like was already set");
@@ -62,12 +56,7 @@ public class FilmServiceImpl implements FilmService {
 
     @Override
     public Film removeLike(int filmId, int userId) {
-        if (filmStorage.getById(filmId) == null) {
-            throw new NotFoundException("No film with such id");
-        }
-        if (userService.getById(userId) == null) {
-            throw new NotFoundException("No user with such id");
-        }
+        userService.getById(userId);
         Film film = filmStorage.getById(filmId);
         if (!film.getLikes().contains(userId)) {
             throw new RepeatException("No likes by user with same id");
@@ -79,10 +68,9 @@ public class FilmServiceImpl implements FilmService {
 
     @Override
     public List<Film> getTopPopularFilms(int count) {
-        return filmStorage.findAll().stream().filter(film -> !film.getLikes().isEmpty())
-                .sorted((o1, o2) -> {
-                    return Integer.compare(o2.getLikes().size(), o1.getLikes().size());
-                })
+        return filmStorage.findAll().stream()
+                .filter(film -> !film.getLikes().isEmpty())
+                .sorted((o1, o2) -> Integer.compare(o2.getLikes().size(), o1.getLikes().size()))
                 .limit(count)
                 .collect(Collectors.toList());
     }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
@@ -2,15 +2,12 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.ecxeption.NotFoundException;
 import ru.yandex.practicum.filmorate.ecxeption.RepeatException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -41,12 +38,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User addFriend(int userId, int friendId) {
-        if (userStorage.getById(userId) == null) {
-            throw new NotFoundException("No User with such id");
-        }
-        if (userStorage.getById(friendId) == null) {
-            throw new NotFoundException("No friend with such id");
-        }
         User user = userStorage.getById(userId);
         User friend = userStorage.getById(friendId);
         if (user.getFriendsId().contains(friendId) || friend.getFriendsId().contains(userId)) {
@@ -61,12 +52,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User removeFriend(int userId, int friendId) {
-        if (userStorage.getById(userId) == null) {
-            throw new NotFoundException("No User with such id");
-        }
-        if (userStorage.getById(friendId) == null) {
-            throw new NotFoundException("No friend with such id");
-        }
         User user = userStorage.getById(userId);
         User friend = userStorage.getById(friendId);
         if (user.getFriendsId().isEmpty()) {
@@ -84,22 +69,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getSameFriends(int userId, int friendId) {
-        if (userStorage.getById(userId) == null) {
-            throw new NotFoundException("No User with such id");
-        }
-        if (userStorage.getById(friendId) == null) {
-            throw new NotFoundException("No friend with such id");
-        }
         User user = userStorage.getById(userId);
         User friend = userStorage.getById(friendId);
-        Set<Integer> userFriendsIds = user.getFriendsId();
-        Set<Integer> friendFriendsIds = friend.getFriendsId();
-        Set<Integer> sameIds = new HashSet<>();
-        for (Integer userFriendsId : userFriendsIds) {
-            if (friendFriendsIds.contains(userFriendsId)) {
-                sameIds.add(userFriendsId);
-            }
-        }
+        List<Integer> sameIds = user.getFriendsId().stream()
+                .filter(friend.getFriendsId()::contains)
+                .collect(Collectors.toList());
         return userStorage.findAll().stream()
                 .filter(u -> sameIds.contains(u.getId()))
                 .collect(Collectors.toList());
@@ -107,12 +81,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getFriends(int userId) {
-        if (userStorage.getById(userId) == null) {
-            throw new NotFoundException("No User with such id");
-        }
-        Set<Integer> friendsIds = userStorage.getById(userId).getFriendsId();
+        User user = userStorage.getById(userId);
         return userStorage.findAll().stream()
-                .filter(user -> friendsIds.contains(user.getId()))
+                .filter(u -> user.getFriendsId().contains(u.getId()))
                 .collect(Collectors.toList());
     }
+
 }
