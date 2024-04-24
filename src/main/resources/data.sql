74b93b5d4bcc8eeab8e4d056a3a28e33b35699b6
LuckyLoki <luckyloki666@ya.ru>
2024-04-24 12:00:00 +0400

    11.1.2 add db h2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,9 +5,9 @@ insert into ratings (name) values ('PG-13');
 insert into ratings (name) values ('R');
 insert into ratings (name) values ('NC-17');
 -- 9. add genres
-insert into genres (name) values ('Комедия');
-insert into genres (name) values ('Драма');
-insert into genres (name) values ('Мультфильм');
-insert into genres (name) values ('Триллер');
-insert into genres (name) values ('Документальный');
-insert into genres (name) values ('Боевик');
+insert into genres (name) values ('Comedy');
+insert into genres (name) values ('Drama');
+insert into genres (name) values ('Cartoon');
+insert into genres (name) values ('Thriller');
+insert into genres (name) values ('Documentary');
+insert into genres (name) values ('Action');
