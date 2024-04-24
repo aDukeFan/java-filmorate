0ac6ae8918efc19f00836b5a1c2251e1ed0c8d27
LuckyLoki <luckyloki666@ya.ru>
2024-04-24 10:07:46 +0400

    11.0.7 Промежуточное задание месяца SQL → Урок 1/2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- 8. add ratings
+insert into ratings (name) values ('G');
+insert into ratings (name) values ('PG');
+insert into ratings (name) values ('PG-13');
+insert into ratings (name) values ('R');
+insert into ratings (name) values ('NC-17');
+-- 9. add genres
+insert into genres (name) values ('Комедия');
+insert into genres (name) values ('Драма');
+insert into genres (name) values ('Мультфильм');
+insert into genres (name) values ('Триллер');
+insert into genres (name) values ('Документальный');
+insert into genres (name) values ('Боевик');
