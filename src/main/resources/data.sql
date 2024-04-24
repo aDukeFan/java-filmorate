a8b3b634d752a22ab9b62582b4addac6aeefac63
LuckyLoki <luckyloki666@ya.ru>
2024-04-24 12:17:37 +0400

    11.1.6 add db h2 fix style

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 
-insert into ratings (name) values (g);
-insert into ratings (name) values (pg);
-insert into ratings (name) values (pg-13);
-insert into ratings (name) values (r);
-insert into ratings (name) values (nc-17);
+insert into ratings (name) values ('g');
+insert into ratings (name) values ('pg');
+insert into ratings (name) values ('pg13');
+insert into ratings (name) values ('r');
+insert into ratings (name) values ('nc17');
 
-insert into genres (name) values (comedy);
-insert into genres (name) values (drama);
-insert into genres (name) values (cartoon);
-insert into genres (name) values (thriller);
-insert into genres (name) values (documentary);
-insert into genres (name) values (action);
+insert into genres (name) values ('comedy');
+insert into genres (name) values ('drama');
+insert into genres (name) values ('cartoon');
+insert into genres (name) values ('thriller');
+insert into genres (name) values ('documentary');
+insert into genres (name) values ('action');
