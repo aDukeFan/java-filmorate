84e79f2bf13088dc9ce220765305aa6d5ff51d29
LuckyLoki <luckyloki666@ya.ru>
2024-04-24 12:04:55 +0400

    11.1.3 add db h2 fix style

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
--- 8. add ratings
+
 insert into ratings (name) values ('G');
 insert into ratings (name) values ('PG');
 insert into ratings (name) values ('PG-13');
 insert into ratings (name) values ('R');
 insert into ratings (name) values ('NC-17');
--- 9. add genres
+
 insert into genres (name) values ('Comedy');
 insert into genres (name) values ('Drama');
 insert into genres (name) values ('Cartoon');
