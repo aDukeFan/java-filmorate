7b32994f8069982420a560334f6ca8e4ce02cebf
LuckyLoki <luckyloki666@ya.ru>
2024-04-24 13:06:53 +0400

    11.1.12 add db h2 fix genres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,9 +5,9 @@ insert into ratings (name) values ('PG-13');
 insert into ratings (name) values ('R');
 insert into ratings (name) values ('NC-17');
 
-insert into genres (name) values ('comedy');
-insert into genres (name) values ('drama');
-insert into genres (name) values ('cartoon');
-insert into genres (name) values ('thriller');
-insert into genres (name) values ('documentary');
-insert into genres (name) values ('action');
+insert into genres (name) values ('Комедия');
+insert into genres (name) values ('Драма');
+insert into genres (name) values ('Мультфильм');
+insert into genres (name) values ('Триллер');
+insert into genres (name) values ('Документальный');
+insert into genres (name) values ('Боевик');
